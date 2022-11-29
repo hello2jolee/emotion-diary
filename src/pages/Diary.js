00dadaed18f93b9a71259b15c068b4b75909fbe2
jioun.lee.dev <jioun.lee@nts-corp.com>
2022-11-29 21:48:20 +0900

    route 2

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,8 +1,15 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-    return <div>
-        <h2>Diary</h2>
-        <p>이곳은 다이어리 입니다</p>
+  const { id } = useParams();
+  console.log(id);
+
+  return (
+    <div>
+      <h2>Diary</h2>
+      <p>이곳은 다이어리 입니다</p>
     </div>
-}
+  );
+};
 
 export default Diary;
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,34 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
-    return <div>
-        <h2>EDIT</h2>
-        <p>이곳은 수정 입니다</p>
+  const navigate = useNavigate(); // 로그인 안 한 사용자가 로그인하는 페이지로 이동할 때 주로 사용
+  const [searchParams, setSearchParams] = useSearchParams(); // [] : 배열 비구조 할당
+
+  const id = searchParams.get("id");
+  console.log(id);
+  const mode = searchParams.get("mode");
+  console.log(mode);
+  return (
+    <div>
+      <h2>EDIT</h2>
+      <p>이곳은 수정 입니다</p>
+      <button onClick={() => setSearchParams({ who: "jolee" })}>바꾸기</button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+            <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
-}
+  );
+};
 
 export default Edit;
